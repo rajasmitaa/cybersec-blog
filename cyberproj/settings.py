68c330681beb186897489e717a62736d183e6357
GauriRocksiess <gauri03uk@gmail.com>
2025-12-05 00:21:00 +0530

    Scary change. Make or break. Removed import dotenv. Reimport foor local server

--- cyberproj/settings.py
+++ cyberproj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
 load_dotenv()
