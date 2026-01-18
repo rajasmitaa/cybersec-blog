aa0ecbef33596bead52609675866b7fa91eef06b
Satwik Mandal <140923984+SatwikMandal@users.noreply.github.com>
2026-01-18 12:38:13 +0530

    Resolved page redirection

--- cyberproj/settings.py
+++ cyberproj/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #something copilot asked me to add
 STATICFILES_DIRS = [
-    BASE_DIR / 'blog' / 'static',
+   BASE_DIR / 'blog' / 'static',
 ]
 
 # Default primary key field type
